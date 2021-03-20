2b22f30c55e5b3444b1ebfb7ecd3e36fc613be83
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-20 14:58:52 +0530

    adding venue controller

--- Src/MovieTicket.Respository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MovieTicket.Respository
-{
-    public class Class1
-    {
-    }
-}
---
+++ Src/WebApi/Controllers/Venues/VenuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Respository.Models.Venues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers.Venues
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VenuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VenueModel> Get()
+        {
+            return new List<VenueModel>();
+        }
+
+        [HttpPost]
+        public IActionResult Post(VenueModel venueModel)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
