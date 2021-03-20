1f256344626fa3dc5f8a97cdf9fba4b23a29f9a7
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-20 14:43:14 +0530

    adding health chckck endpoint for API

---
+++ Src/WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("hc")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return DateTime.UtcNow.ToString();
+        }
+    }
+}
